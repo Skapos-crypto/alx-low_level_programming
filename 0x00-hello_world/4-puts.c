509dff0ad8470d20ba91a62792376aa265447b1e
mohamed bahgat <mohamedbahgat2003@gmail.com>
2023-07-23 10:34:39 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-
-int main()
+/*the int main */
+int main(void)
 {
-	char mystr[]="Programming is like building a multilingual puzzle";
+	char mystr[] = ""Programming is like building a multilingual puzzle";
 		
 	puts(mystr);
 
 
 		return 0;
-
-
 
 
 
